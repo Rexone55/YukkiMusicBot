a742e92b090584fae921b28d286ab9d6b86ed803
Deshadeeth Thisarana <77653060+Deshadeeth-Thisarana@users.noreply.github.com>
2021-12-27 08:07:08 +0530

    Done

--- Yukki/Plugins/Play.py
+++ Yukki/Plugins/Play.py
@@ -119,23 +119,49 @@ async def play(_, message: Message):
             return
         mystic = await message.reply_text("🔍 **Searching**...")
         query = message.text.split(None, 1)[1]
-        (
-            title,
-            duration_min,
-            duration_sec,
-            thumb,
-            videoid,
-        ) = get_yt_info_query(query)
+        user_id = message.from_user.id
+        
+        results = YoutubeSearch(query, max_results=5).to_dict()
+        photo = "https://telegra.ph/file/5321556a2d8295ba684f8.jpg"
+        caption=(f"1️⃣ <b>{results[0]['title']}</b>\n └ 💡 [More information](https://t.me/{BOT_USERNAME}?start=info_{results[0]['id']})\n\n2️⃣ <b>{results[1]['title']}</b>\n └ 💡 [More information](https://t.me/{BOT_USERNAME}?start=info_{results[1]['id']})\n\n3️⃣ <b>{results[2]['title']}</b>\n └ 💡 [More information](https://t.me/{BOT_USERNAME}?start=info_{results[3]['id']})\n\n4️⃣ <b>{results[3]['title']}</b>\n └ 💡 [More information](https://t.me/{BOT_USERNAME}?start=info_{results[3]['id']})\n\n5️⃣ <b>{results[4]['title']}</b>\n └ 💡 [More information](https://t.me/{BOT_USERNAME}?start=info_{results[4]['id']})")
+        buttons = search_markup(
+            results[0]["id"],
+            results[1]["id"],
+            results[2]["id"],
+            results[3]["id"],
+            results[4]["id"],
+            results[0]["duration"],
+            results[1]["duration"],
+            results[2]["duration"],
+            results[3]["duration"],
+            results[4]["duration"],
+            user_id,
+            query,
+        )
         await mystic.delete()
-        buttons = url_markup(
-            videoid, duration_min, message.from_user.id, query, 0
-        )
-        mention = f"[{message.from_user.first_name}](tg://user?id={message.from_user.id})"
         return await message.reply_photo(
-            photo=thumb,
-            caption=f"🏷 **Name:**{title}\n**⏱Duration**: {duration_min} Mins\n🎧 **Request by:**{mention}\n\n[Get  Information](https://t.me/{BOT_USERNAME}?start=info_{videoid})",
-            reply_markup=InlineKeyboardMarkup(buttons),
+          photo=photo,
+          caption=caption,
+          reply_markup=InlineKeyboardMarkup(buttons)
         )
+
+#        (
+#            title,
+#            duration_min,
+#            duration_sec,
+#            thumb,
+#            videoid,
+#        ) = get_yt_info_query(query)
+#        await mystic.delete()
+#        buttons = url_markup(
+#            videoid, duration_min, message.from_user.id, query, 0
+#        )
+#        mention = f"[{message.from_user.first_name}](tg://user?id={message.from_user.id})"
+#        return await message.reply_photo(
+#            photo=thumb,
+#            caption=f"🏷 **Name:**{title}\n**⏱Duration**: {duration_min} Mins\n🎧 **Request by:**{mention}\n\n[Get  Information](https://t.me/{BOT_USERNAME}?start=info_{videoid})",
+#            reply_markup=InlineKeyboardMarkup(buttons),
+#        )
 
 
 @app.on_callback_query(filters.regex(pattern=r"Yukki"))
